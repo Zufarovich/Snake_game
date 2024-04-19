09a3df59888f3c4e5b09853b7b6498c7f3f871ba
Amir <shamsutdinov.az@phystech.edu>
2024-04-19 18:23:53 +0300

    Fixed abbility to close the window in GView

--- Gview.cpp
+++ Gview.cpp
@@ -82,7 +82,7 @@ void GView::mainloop()
     {
         sf::Event event;
         while(window.pollEvent(event))
-            if(event.type = sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
                 window.close();
 
         window.clear();
