71fa8450a04c016f24d91004192e7f9d7f5f7bca
Amir <shamsutdinov.az@phystech.edu>
2024-04-19 18:21:40 +0300

    Added options for text or gview

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include "view.hpp"
 #include "game.hpp"
 #include "controller.hpp"
+#include <getopt.h>
+#include <unistd.h>
 
-int main()
+struct option long_options[] = {
+	{"text_view",		 no_argument, 0, 't' },
+	{"graphical_view",  no_argument, 0, 'g' },
+};
+
+int main(int argc, char* argv[])
 {
-	View* view = View::get_view("g_view");
+	int  opt          = '\0';
+	int  option_index = 0;
+	View* view;
+
+	while((opt = getopt_long(argc, argv, "tg", long_options, &option_index)) != -1)
+	{
+		switch(opt){
+		case 't':
+			view = View::get_view("text_view");
+			break;
+		case 'g':
+		    view = View::get_view("g_view");
+			break;
+		default:
+			view = View::get_view("g_view");
+			printf("with command \"snake --help\" you can get additional information.\n");
+			break;
+		}
+	}	
 	
 	Model model(*view);
 	Control control(model, *(model.snakes.begin()));
